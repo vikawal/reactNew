591ecb122a57e5b9eae5f26e07cb3318de4c4ed9
vikawal <108365967+vikawal@users.noreply.github.com>
2023-04-21 02:19:57 +0200

    added styles for CountryAll information page

--- src/components/CountryAllInfo.jsx
+++ src/components/CountryAllInfo.jsx
@@ -3,6 +3,7 @@ import { useFavorites } from "./FavCountryContext";
 import {ReactComponent as HeartFilled} from './assets/heart-solid.svg';
 import {ReactComponent as HeartEmpty} from './assets/heart-regular.svg';
 // import {useFavoritesHook} from './useHooks/useFavoritesHook';
+import './styles/countryPage.css';
 
 function CountryAllInfo({ country }) {
   const {favorites, toggleFavorite, isFavorited} = useFavorites();
@@ -13,39 +14,45 @@ function CountryAllInfo({ country }) {
   };
   // console.log(country);
   return (
-    <div>
-      {country? (
+    <div className="country-info-container">
+      {country ? (
         country && typeof country.name === "object" && country.name.common ? (
-          <h2>{country.name.common}</h2>
+          <h2 className="name">{country.name.common}</h2>
         ) : (
           <p>Country name is not available</p>
         )
       ) : (
         <p>Loading....</p>
-      )
-      }
-      <p>Flag: {" "}
-        <img src={country.flags.svg} alt={country.name.common} width ="200"/></p>
-          <p>Currency: 
-      {Object.entries(country.currencies).map(([code, currency]) => (
+      )}
+      <p className="flag">
+        <img src={country.flags.svg} alt={country.name.common} width="200" />
+      </p>
+      <p className="currency">
+        Currency:{" "}
+        {Object.entries(country.currencies).map(([code, currency]) => (
           <span key={code}>
             {currency.name} ({currency.symbol})
           </span>
-      ))} </p>
+        ))}
+      </p>
       {country.capital && country.capital.length > 0 ? (
-        <p>Capital: {country.capital[0]}</p>
+        <p className="capital">Capital: {country.capital[0]}</p>
       ) : (
-        <p>Capital: Not available</p>
+        <p className="capital">Capital: Not available</p>
       )}
-      <p>Languages: {Object.values(country.languages).join(", ")}</p>
-      <p>Region: {country.region}</p>
-      <p>Subregion: {country.subregion}</p>
-      <p>Borders: {country.borders.join(", ")}</p>
-      <p>Population: {country.population}</p>
+      <p className="language">Languages: {Object.values(country.languages).join(", ")}</p>
+      <p className="region">Region: {country.region}</p>
+      <p className="subregion">Subregion: {country.subregion}</p>
+      <p className="borders">Borders: {country.borders.join(", ")}</p>
+      <p className="population">Population: {country.population}</p>
       <div>
-      <button style={{ border: "none", background: "none" }} onClick={handleFavoriteClick}>
-        {isFavorited(country.cca3) ? <HeartFilled width="24" height="24" /> : <HeartEmpty width="24" height="24"  />}
-       </button>
+        <button className="favButton" onClick={handleFavoriteClick}>
+          {isFavorited(country.cca3) ? (
+            <HeartFilled className="favoriteIcon" />
+          ) : (
+            <HeartEmpty className="favoriteIcon" />
+          )}
+        </button>
       </div>
     </div>
   );
