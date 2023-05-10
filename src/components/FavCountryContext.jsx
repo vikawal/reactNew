30e5e0c82a0588386a508a18b8d7d637788559fc
vikawal <108365967+vikawal@users.noreply.github.com>
2023-05-11 00:31:02 +0200

    favorites work

--- src/components/FavCountryContext.jsx
+++ src/components/FavCountryContext.jsx
@@ -5,17 +5,37 @@ export const FavCountryContext = createContext();
 export function FavCountryProvider({ children }) {
   const [favorites, setFavorites] = useState([]);
 
+  // function toggleFavorite(country) {
+  //   if (favorites.some(fav => fav.cca3 === country.cca3)) {
+  //     const updatedFavorites = favorites.filter((fav) => fav.cca3 !== country.cca3);
+  //     setFavorites(updatedFavorites);
+  //     console.log ('Favorites after removal:', updatedFavorites);
+  //   } else {
+  //     const updatedFavorites = [...favorites, country]
+  //     setFavorites(updatedFavorites);
+  //     console.log('Favorites after addition:', updatedFavorites);
+  //   }
+  // }
+
   function toggleFavorite(countryCode) {
     if (favorites.includes(countryCode)) {
-      setFavorites(favorites.filter((cca3) => cca3 !== countryCode));
+      const updatedFavorites = favorites.filter((cca3) => cca3 !== countryCode);
+      setFavorites(updatedFavorites);
+      console.log ('Favorites after removal:', updatedFavorites);
     } else {
-      setFavorites([...favorites, countryCode]);
+      const updatedFavorites = [...favorites, countryCode]
+      setFavorites(updatedFavorites);
+      console.log('Favorites after addition:', updatedFavorites);
     }
   }
 
   function isFavorited(countryCode) {
     return favorites.includes(countryCode);
   }
+
+  // function isFavorited(countryCode) {
+  //   return favorites.some(fav => fav.cca3 === countryCode);
+  // }
 
   return (
     <FavCountryContext.Provider value={{ favorites, toggleFavorite, isFavorited }}>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 import useFetch from './useFetch';
 import './styles/home.css';
 import './styles/spinner.css';
 import { Link } from 'react-router-dom';
 // import { useFavorites } from "./FavCountryContext";
-import { FavCountryContext} from './FavCountryContext.jsx';
+import { FavCountryContext} from './FavCountryContext';
 import { useContext } from 'react';
 import {ReactComponent as HeartFilled} from './assets/heart-solid.svg';
 import {ReactComponent as HeartEmpty} from './assets/heart-regular.svg';
@@ -13,11 +13,12 @@ function Home() {
 
   const {favorites, toggleFavorite, isFavorited} = useContext(FavCountryContext);
 
-
-
+  // const handleFavoriteClick = (country) => {
+  //   toggleFavorite(country); 
+  // };
+ 
   const handleFavoriteClick = (countryCode) => {
     toggleFavorite(countryCode); 
-
   };
   // const [favorites, setFavorites] = useFavorites();
   // const isFavorited = favorites.includes(country.cca3);
@@ -45,7 +46,8 @@ function Home() {
 
   if (loading) return <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>;
   if (error) return <p className="error-message">{error}</p>;
-  console.log(countries);
+  // console.log(countries);
+  console.log(favorites);
 
 
   return (
@@ -93,96 +95,3 @@ function Home() {
 }
 
 export default Home;
-
-// import React, { useState } from 'react';
-// import useFetch from './useFetch';
-// import {Link} from 'react-router-dom';
-// import "./home.css"
-
-// function Home() {
-//   const [currentRegion, setCurrentRegion] = useState('All');
-//   const { data: countries, loading, error } = useFetch(
-//     currentRegion === 'All'
-//       ? 'https://restcountries.com/v3.1/all?fields=name,region,subregion,flags,capital,cca3'
-//       : `https://restcountries.com/v3.1/region/${currentRegion}?fields=name,region,subregion,flags,capital,cca3`
-//   );
-
-//   const regions = ['All', 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
-
-//   const handleRegionClick = (region) => {
-//     setCurrentRegion(region);
-//   };
-
-//   // const handleAllCountriesClick = () => {
-//   //   setCurrentRegion('all');
-//   // };
-
-//   if (loading) return <p>Loading...</p>;
-//   if (error) return <p>{error}</p>;
-
-//   console.log(countries);
-
-//   return (
-//     <div>
-//       <h1>Quiz App. </h1>
-//       <h2>Choose which quiz you want to play from the navigation bar</h2>
-//       {/* <button onClick={handleAllCountriesClick}>All countries</button> */}
-//       {regions.map((region) => (
-//         <button key={region} onClick={() => handleRegionClick(region)}>
-//           {region}
-//         </button>
-//       ))}
-//       {countries.map((country) => (
-//         <div key={country.name.common}>
-//           <Link to={`/country/${country.cca3}`}>
-//             <h2>{country.name.common}</h2>
-//             {country.region && country.region.length > 0 ? (
-//               <p>Region: {country.region}</p>
-//             ) : (
-//               <p>Region: Not available</p>
-//             )}
-//             {country.subregion && country.subregion.length > 0 ? (
-//               <p>Subregion: {country.subregion}</p>
-//             ) : (
-//               <p>Subregion: Not available</p>
-//             )}
-//             {country.capital && country.capital.length > 0 ? (
-//               <p>Capital: {country.capital}</p>
-//             ) : (
-//               <p>Capital: Not available</p>
-//             )}
-//             <img src={country.flags.png} alt={country.name.common} width="200" />
-//           </Link>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default Home;
-// import React from 'react';
-// import useFetch from './useFetch';
-
-// function Home() {
-//   const { data: countries, loading, error } = useFetch('https://restcountries.com/v2/all');
-
-//   if (loading) return <p>Loading...</p>;
-//   if (error) return <p>{error}</p>;
-
-//   return (
-//     <div>
-//       <h1>Quiz App. </h1>
-//       <h2>Choose which quizz you want to play from navigation bar</h2>
-//       {countries.slice(0, 10).map(country => (
-//         <div key={country.alpha3Code}>
-//           <h2>{country.name}</h2>
-//           <p>Capital: {country.capital}</p>
-//           <p>Region: {country.region}</p>
-//           <img src={country.flag} alt={country.name} width="200" />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default Home;
